86e872da26ed5d1153300a23045a2a724c1aef6f
TarcaAndreea <adrianatarca1@gmail.com>
2024-01-14 01:35:55 +0200

    UPDATE LOADER

--- src/Pages/RegisterPage.jsx
+++ src/Pages/RegisterPage.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Typography, Box, TextField } from '@mui/material';
+import { Button, Typography, Box, TextField, Container } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
 import { useDispatch, useSelector } from 'react-redux';
 import { register } from 'Redux/Operation';
 import { useNavigate } from 'react-router-dom';
 import { selectIsAuthenticated } from 'Redux/selectors';
 import ResponsiveAppBar from 'components/NavBar/NavBar';
 import HomePageDesign from 'components/Design/HomePageDesign';
+import Loader from 'components/Loader/Loader';
 export default function RegisterPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -36,49 +38,98 @@ export default function RegisterPage() {
   return (
     <HomePageDesign>
       <ResponsiveAppBar />
-      <Box
-        onSubmit={handleSubmit}
-        component="form"
-        sx={{
-          marginTop: '20%',
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box>
+          <Typography
+            component="h1"
+            variant="h5"
+            fontSize="14px"
+            fontFamily="Verdana"
+            fontWeight="700"
+            textTransform="uppercase"
+            color="rgba(252, 132, 45, 1)"
+          >
+            Log in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              variant="standard"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              variant="standard"
+            />
+            <Box>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  fontFamily: 'Verdana',
+                  fontSize: '14px',
+                  textTransform: 'unset',
+                  borderRadius: '30px',
+                  boxShadow: '0px 4px 10px 0px rgba(252, 132, 45, 0.5)',
+                  fontWeight: '700',
+                  width: '210px',
+                  height: '43px',
+                  marginTop: '35px',
+                  backgroundColor: 'rgba(252, 132, 45, 1)',
+                }}
+              >
+                {isAuth === 'login' ? <Loader /> : <>Log In</>}
+              </Button>
 
-          padding: '30px',
-
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: '20px',
-        }}
-      >
-        <Typography variant="h4" sx={{ fontSize: '30px', color: 'gray' }}>
-          Register
-        </Typography>
-        <TextField
-          type="name"
-          name="name"
-          placeholder="Name"
-          variant="standard"
-          fullWidth
-          onChange={handleChangeInput}
-        />
-        <TextField
-          type="email"
-          name="email"
-          variant="standard"
-          fullWidth
-          onChange={handleChangeInput}
-        />
-        <TextField
-          type="password"
-          name="password"
-          variant="standard"
-          fullWidth
-          onChange={handleChangeInput}
-        />
-        <Button type="submit" variant="contained">
-          Register
-        </Button>
-      </Box>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  color: 'rgba(252, 132, 45, 1)',
+                  fontFamily: 'Verdana',
+                  fontSize: '14px',
+                  textTransform: 'unset',
+                  borderRadius: '30px',
+                  boxShadow: '0px 4px 10px 0px rgba(252, 132, 45, 0.5)',
+                  fontWeight: '700',
+                  width: '210px',
+                  height: '43px',
+                  marginTop: '35px',
+                  backgroundColor: 'white',
+                }}
+                onClick={() => navigate('/register')}
+              >
+                <>Register</>
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
     </HomePageDesign>
   );
 }
---
+++ src/components/Loader/loader.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { CircularProgress } from '@mui/material';
+export default function Loader() {
+  return (
+    <div>
+      <CircularProgress />
+    </div>
+  );
+}
