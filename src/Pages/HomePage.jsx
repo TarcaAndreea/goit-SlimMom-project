feb8d6333e15526ac30309a9ee4dc98269ea7350
TarcaAndreea <adrianatarca1@gmail.com>
2024-01-14 16:57:55 +0200

    up

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ResponsiveAppBar from 'components/NavBar/NavBar';
 import HomePageDesign from 'components/Design/HomePageDesign';
-import './design.module.css';
+
 import DailyCalorieIntake from 'components/DailyCalorieIntake/dailyCalorieIntake';
 export default function HomePage() {
   return (
     <div className="homepage">
       <HomePageDesign>
-        <ResponsiveAppBar className="navBar" />
+        <ResponsiveAppBar />
         <DailyCalorieIntake />
       </HomePageDesign>
     </div>
